0722eee8d46faf425ac7b5afa32fb97fcdc159e1
Traili <154281648+trailios@users.noreply.github.com>
2024-02-20 16:54:54 +0100

    Update TKclaimer.py
    made it 1.5x faster

--- TKclaimer.py
+++ TKclaimer.py
@@ -24,11 +24,16 @@ startupembed = {
         {
             "title": "Started!",
             "description": f"TK - claimer V{VERSION}\n\nTotal cookies: {len(cookies)}\nChannel: <#{CHANNEL_ID}>\n\nLogs: <#{lchannel}>\nDetetions: <#{dchannel}>\nErrors: <#{echannel}>",
-            "footer": {"text": "Made by: TK aka Traili your favourite developer", "icon_url": "https://cdn.discordapp.com/avatars/1137484045501092012/4ea0960613ba5ac4dc18ccc95fe34b70.webp?size=1024&format=webp&width=0&height=256",},
-            "thumbnail": {"url": "https://cdn.discordapp.com/avatars/1137484045501092012/4ea0960613ba5ac4dc18ccc95fe34b70.webp?size=1024&format=webp&width=0&height=256"},
+            "footer": {
+                "text": "Made by: TK aka Traili your favourite developer",
+                "icon_url": "https://cdn.discordapp.com/avatars/1137484045501092012/4ea0960613ba5ac4dc18ccc95fe34b70.webp?size=1024&format=webp&width=0&height=256",
+            },
+            "thumbnail": {
+                "url": "https://cdn.discordapp.com/avatars/1137484045501092012/4ea0960613ba5ac4dc18ccc95fe34b70.webp?size=1024&format=webp&width=0&height=256"
+            },
             "color": 0x3498DB,
         }
-    ]
+    ],
 }
 
 requests.post(logs, json=startupembed)
@@ -118,18 +123,21 @@ def checkgroup(groupid, cookie, xcsrf_token):
         robux = resp2.json()["robux"]
         revenue = resp3.json()["pendingRobux"]
         embed = {
+            "username": "TK - Detections",
             "embeds": [
                 {
                     "title": f"{name}",
-                    "description": f"Members: {members}\nRobux: {robux}\nPending: {revenue}",
+                    "description": f"Members: {members}\nRobux: {robux}\nRevenue: {revenue}",
                     "footer": {
                         "text": "Made by: TK aka Traili your favourite developer",
                         "icon_url": "https://cdn.discordapp.com/avatars/1137484045501092012/4ea0960613ba5ac4dc18ccc95fe34b70.webp?size=1024&format=webp&width=0&height=256",
                     },
-                    "thumbnail": {"url": "https://cdn.discordapp.com/avatars/1137484045501092012/4ea0960613ba5ac4dc18ccc95fe34b70.webp?size=1024&format=webp&width=0&height=256"},
+                    "thumbnail": {
+                        "url": "https://cdn.discordapp.com/avatars/1137484045501092012/4ea0960613ba5ac4dc18ccc95fe34b70.webp?size=1024&format=webp&width=0&height=256"
+                    },
                     "color": 0x3498DB,
                 }
-            ]
+            ],
         }
         requests.post(detections, json=embed)
     except Exception:
@@ -142,7 +150,9 @@ def checkgroup(groupid, cookie, xcsrf_token):
                         "text": "Made by: TK aka Traili your favourite developer",
                         "icon_url": "https://cdn.discordapp.com/avatars/1137484045501092012/4ea0960613ba5ac4dc18ccc95fe34b70.webp?size=1024&format=webp&width=0&height=256",
                     },
-                    "thumbnail": {"url": "https://cdn.discordapp.com/avatars/1137484045501092012/4ea0960613ba5ac4dc18ccc95fe34b70.webp?size=1024&format=webp&width=0&height=256"},
+                    "thumbnail": {
+                        "url": "https://cdn.discordapp.com/avatars/1137484045501092012/4ea0960613ba5ac4dc18ccc95fe34b70.webp?size=1024&format=webp&width=0&height=256"
+                    },
                     "color": 0x3498DB,
                 }
             ]
@@ -161,23 +171,24 @@ def claim(groupid, cookie, xcsrf_token):
     response = client.post(url, headers=headers)
     return response
 
+
 def leave(groupid, cookie, xcsrf_token):
     headers = {
         "Content-Type": "application/json",
         "Cookie": f"GuestData=UserID=-1458690174; .ROBLOSECURITY={cookie}; RBXEventTrackerV2=CreateDate=11/19/2023 12:07:42 PM&rbxid=5189165742&browserid=200781876902;",
         "x-csrf-token": f"{xcsrf_token}",
     }
-    res2 = requests.get(f"https://users.roblox.com/v1/users/authenticated", headers=headers)
+    res2 = requests.get(
+        f"https://users.roblox.com/v1/users/authenticated", headers=headers
+    )
     userid = res2.json()["id"]
     url = f"{api}/v1/groups/{groupid}/users/{userid}"
     response = requests.delete(url, headers=headers)
     return response
 
-async def main(groupid):
-    global cookies
-    cookie = random.choice(cookies)
+
+async def main(groupid, cookie, xcsrf_token):
     time1 = time.time()
-    xcsrf_token = await xtoken(cookie)
     join1 = await join(groupid, cookie, xcsrf_token)
     claim1 = await claim(groupid, cookie, xcsrf_token)
     time2 = time.time()
@@ -187,12 +198,14 @@ async def main(groupid):
             "embeds": [
                 {
                     "title": f"Claimed group: {groupid}",
-                    "description": f"traili doesnt have detection cause nigga is lazy\nJoined and claimed in {round(time2 - time1, 2)} seconds",
+                    "description": f"Detection will be send too <#{dchannel}>\nJoined and claimed in {round(time2 - time1, 2)} seconds",
                     "footer": {
                         "text": "made by the only traili there is",
                         "icon_url": "https://cdn.discordapp.com/avatars/1137484045501092012/4ea0960613ba5ac4dc18ccc95fe34b70.webp?size=1024&format=webp&width=0&height=256",
                     },
-                    "thumbnail": {"url": "https://cdn.discordapp.com/avatars/1137484045501092012/4ea0960613ba5ac4dc18ccc95fe34b70.webp?size=1024&format=webp&width=0&height=256"},
+                    "thumbnail": {
+                        "url": "https://cdn.discordapp.com/avatars/1137484045501092012/4ea0960613ba5ac4dc18ccc95fe34b70.webp?size=1024&format=webp&width=0&height=256"
+                    },
                     "color": 0x3498DB,
                 }
             ]
@@ -205,12 +218,14 @@ async def main(groupid):
             "embeds": [
                 {
                     "title": f"Failed to claim group: {groupid}",
-                    "description": f"traili doesnt have detection cause nigga is lazy\n\nStatus: join -> {join1.status_code} | claim -> {claim1.status_code}\n\nErrors: {await join1.text()} | {await claim1.text()}",
+                    "description": f"i was to slow..? no this cant be\n\nStatus: join -> {join1.status_code} | claim -> {claim1.status_code}\n\nErrors: {await join1.text()} | {await claim1.text()}",
                     "footer": {
                         "text": "made by the only traili there is",
                         "icon_url": "https://cdn.discordapp.com/avatars/1137484045501092012/4ea0960613ba5ac4dc18ccc95fe34b70.webp?size=1024&format=webp&width=0&height=256",
                     },
-                    "thumbnail": {"url": "https://cdn.discordapp.com/avatars/1137484045501092012/4ea0960613ba5ac4dc18ccc95fe34b70.webp?size=1024&format=webp&width=0&height=256"},
+                    "thumbnail": {
+                        "url": "https://cdn.discordapp.com/avatars/1137484045501092012/4ea0960613ba5ac4dc18ccc95fe34b70.webp?size=1024&format=webp&width=0&height=256"
+                    },
                     "color": 0x3498DB,
                 }
             ]
@@ -223,7 +238,7 @@ async def main(groupid):
             print_error(f"Failed to leave group: {groupid}")
 
 
-async def getgroup():
+async def getgroup(cookie, xcsrf_token):
     global latest_message_id
 
     headers = {"authorization": f"{DISCORD_TOKEN}"}
@@ -246,8 +261,8 @@ async def getgroup():
             content1 = jsonn[0]["content"]
             numbers_only = re.findall(r"\d+", content1)
 
-            if "roblox.com/groups/" in content1 or "www.roblox.com/groups/" in content1:
-                await main(numbers_only[0])
+            if "roblox.com/groups/" in content1:
+                await main(numbers_only[0], cookie, xcsrf_token)
             else:
                 ...
 
@@ -259,8 +274,12 @@ async def getgroup():
 
 
 async def check():
+    global cookies
     while True:
-        await getgroup()
+        cookie = random.choice(cookies)
+        xcsrf_token = await xtoken(cookie)
+        await getgroup(cookie, xcsrf_token)
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(check())
